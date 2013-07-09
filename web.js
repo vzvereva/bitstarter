40d0aa5973845666b799ebb3af69a396d824f96e
Vera Zvereva <vera.zvereva.83@gmail.com>
2013-07-09 19:48:52 +0000

    Read index.html file

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var respMessage = fs.readFileSync('index.html').toString();
+  response.send(respMessage);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
